7e1de7b5ab01124cd2dda6483a94d2a0f65fb444
Adamy <admye@ucdavis.edu>
2025-01-23 04:32:36 -0800

    navbar & bg

--- frontend/mars-front/src/app/_components/navbar.tsx
+++ frontend/mars-front/src/app/_components/navbar.tsx
@@ -1,13 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Button className="navbar-brand" >
-                <Link href="/">Home</Link>
-            </Button>
+        <nav className="bg-white shadow-md fixed w-full z-10 top-0">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between h-16 items-center">
+            <div className="flex text-xl font-bold text-gray-800">
+                Mars Visit Application Form
+            </div>
+            <div className="flex-shrink-0">
+                <Button className="text-white bg-gray-100 hover:bg-gray-300">
+                <Link href="/" className="text-xl text-gray-800 hover:text-gray-600">
+                    Home
+                </Link>
+                </Button>
+            </div>
+            </div>
+        </div>
         </nav>
     );
 }
--- frontend/mars-front/src/app/page.tsx
+++ frontend/mars-front/src/app/page.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { url } from "inspector";
 
 export default function Home() {
   return (
-    <div className="text-center container mx-auto px-4">
-      <h1 className="text-4xl font-bold mt-8">Welcome to Mars</h1>
-      <p className="mt-4">The best place for software engineers</p>
-      <p className="mt-4">Join us now!</p>
-      <Button className="mt-4">
-        <Link href="/apply"> Apply </Link>
-      </Button>
+    <div className="text-center container p-0 m-0 mx-auto items-center justify-center h-screen bg-[url('../mars.jpg')] bg-cover bg-center ">
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-4xl font-bold mt-8">Welcome to Mars</h1>
+        <p className="mt-4">The best place for software engineers</p>
+        <p className="mt-4">Apply now!</p>
+        <Link href="/apply">
+            <Button className="mt-4">Apply</Button>
+        </Link>
+      </div>
     </div>
   );
 }
