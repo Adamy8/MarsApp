bc97a26e637a1cfa2815d2de0fcb31d1048bc171
Adamy <admye@ucdavis.edu>
2025-01-26 14:22:34 -0800

    basepath for future

--- frontend/mars-front/next.config.ts
+++ frontend/mars-front/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // // Adding basePath and assetPrefix (for future use)
+  // basePath: '/marsapp',
+  // assetPrefix: '/marsapp',
+
   async rewrites() {
     const apiBaseUrl = process.env.NODE_PUBLIC_API_URL || 'http://localhost:4000';
     return [
